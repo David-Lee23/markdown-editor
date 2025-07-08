5ea9348ebbc38ba766869e2d8af12bf0da32ca0b
David Lee <laxdlee@gmail.com>
2025-07-08 16:53:10 -0400

    added line wrapping and 'drag to resize' editor and previewer sizing

--- src/main.js
+++ src/main.js
@@ -19,3 +19,38 @@ marked.setOptions({
     return hljs.highlightAuto(code).value;
   },
 });
+
+const resizer = document.getElementById("resizer");
+let isResizing = false;
+
+resizer.addEventListener("mousedown", (e) => {
+  isResizing = true;
+  document.body.style.cursor = "col-resize";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isResizing) return;
+
+  //get total width of container
+  const container = document.getElementById("app");
+  const totalWidth = container.offsetWidth;
+
+  //new width
+  const newEditorWidth = e.clientX - container.offsetLeft;
+  const editor = document.getElementById("editor");
+  const preview = document.getElementById("preview");
+
+  //constrain width to 10-90% to avoid layout breaking
+  const minWidth = totalWidth * 0.1;
+  const maxWidth = totalWidth * 0.9;
+
+  if (newEditorWidth > minWidth && newEditorWidth < maxWidth) {
+    editor.style.width = `${newEditorWidth}px`;
+    preview.style.width = `${totalWidth - newEditorWidth - resizer.offsetWidth}px`;
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  isResizing = false;
+  document.body.style.cursor = "default";
+});
